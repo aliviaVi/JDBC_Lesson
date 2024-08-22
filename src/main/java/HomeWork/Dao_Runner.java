d97d1ffcb233de909c810e89196c1b2ca4c4b997
Alina Vialenko <49653661+aliviaVi@users.noreply.github.com>
2024-08-22 10:48:13 +0200

    alina HW

--- src/main/java/HomeWork/Dao_Runner.java
+++ src/main/java/HomeWork/Dao_Runner.java
@@ -10,20 +10,28 @@ import java.util.List;
 public class Dao_Runner {
     public static void main(String[] args) {
         InvoiceDao invoiceDao = InvoiceDao.getInstance();
-//        List<Invoice> allInvoices = invoiceDao.findAll();
+        List<Invoice> allInvoices = invoiceDao.findAll();
 //
-//        allInvoices.forEach(invoice -> System.out.println(invoice));
+        allInvoices.forEach(invoice -> System.out.println(invoice));
 //
-        Invoice invoice = new Invoice(20,"333-33-33", 2,
+/*        Invoice invoice = new Invoice(20,"333-33-33", 2,
                 BigDecimal.valueOf(222.22),BigDecimal.valueOf(222.22),
                 LocalDate.of(2024,02,19),
                 LocalDate.of(2024,05,25),
                 LocalDate.of(2024,04,01));
         invoiceDao.save(invoice);
+*/
 
-//        Invoice invoice = new Invoice(21,"222-222-22",3, BigDecimal.valueOf(222.22),
-//                BigDecimal.valueOf(222.22), LocalDate.of(2024,01,01),
-//                LocalDate.of(2024,02,02),LocalDate.of(2024,01,01));
-//        invoiceDao.save(invoice);
+
+//        invoiceDao.update(3);
+//        invoiceDao.deleteByInvoiceID(1);
+//        invoiceDao.deleteByClientId(2);
+        System.out.println(invoiceDao.findById(18));
+        System.out.println("after update");
+//        List<Invoice> allInvoices2 = invoiceDao.findAll();
+//
+ //       allInvoices2.forEach(invoice -> System.out.println(invoice));
+
+
     }
 }
--- src/main/java/HomeWork/dao/Dao.java
+++ src/main/java/HomeWork/dao/Dao.java
@@ -5,15 +5,18 @@ import HomeWork.entity.Invoice;
 import java.util.List;
 import java.util.Optional;
 
-public interface Dao<K,E> {
+public interface Dao<K, E> {
 
     List<E> findAll();
 
     void save(E entity);
 
-    void update(E entity);
+    // void update(E entity);
+    void update(K id);
 
-    void deleteById(K id);
+    void deleteByClientId(K id);
+
+    void deleteByInvoiceID(K id);
 
     Optional<E> findById(K id);
 
